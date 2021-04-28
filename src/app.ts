9d4fc14ce6cfff7103fd82398f6c8192ef309ae9
LuizSouz404 <luiz.souz404@gmail.com>
2021-04-28 16:00:13 -0300

    CHORE: add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'reflect-metadata';
 import './database';
+import cors from 'cors';
 
 import { router } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
