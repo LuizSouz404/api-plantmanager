53299efde7f98d66e511111907418949ee9459ea
LuizSouz404 <luiz.souz404@gmail.com>
2021-05-14 11:21:59 -0300

    terst

--- build/server.js
+++ build/server.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
 var app_1 = require("./app");
+dotenv_1.default.config();
 app_1.app.listen(3333, function () {
     console.log('Server is running on port 3333!');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { app } from './app';
+
+dotenv.config();
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333!');
